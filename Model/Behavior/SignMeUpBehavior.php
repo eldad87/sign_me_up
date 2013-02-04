7fdadb1cc27baf6879a8eaf5a195c1b9a50dc237
Eldad Yamin <eldad87@gmail.com>
2013-02-04 23:14:25 +0200

    Paypal password requirements

--- Model/Behavior/SignMeUpBehavior.php
+++ Model/Behavior/SignMeUpBehavior.php
@@ -14,35 +14,52 @@ class SignMeUpBehavior extends ModelBehavior {
 		),*/
 		'email' => array(
 			'validEmail' => array(
-				'rule' => array('email', true),
-				'message' => 'Please supply a valid & active email address'
+				'rule'      => array('email', true),
+				'message'   => 'Please supply a valid & active email address'
 			),
 			'emailExists' => array(
-				'rule' => 'isUnique',
-				'message' => 'Sorry, this email address is already in use'
+				'rule'      => 'isUnique',
+				'message'   => 'Sorry, this email address is already in use',
+                'required'	=> true,
+                'on'        => 'create',
 			),
 		),
 		'password' => array(
-			'match' => array(
-				'rule' => array('confirmPassword', 'password', 'password2'),
-				'message' => 'Passwords do not match'
-			),
+            'match' => array(
+                'rule'      => array('confirmPassword', 'password', 'password2'),
+                'message'   => 'Passwords do not match'
+            ),
+            'confirmPasswordNotAsEmail' => array(
+                'rule'      => array('confirmPasswordNotAsEmail', 'password', 'email'),
+                'message'   => 'Passwords Cannot be identical to your email'
+            ),
 			'minRequirements' => array(
-				'rule' => array('minLength', 6),
-				'message' => 'Passwords need to be at least 6 characters long'
-			)
+				'rule'      => array('minLength', 6),
+				'message'   => 'Passwords need to be at least 6 characters long'
+			),
+            'strength' => array(
+                'rule'      => '/^(?=.*\d)(?=.*[a-z]).{6,}$/i',
+                'message'   => 'Passwords must have at least one alpha and one numeric, min 6 characters'
+            )
 		),
 	);
 
 	public function beforeValidate(Model $Model) {
 		$this->model = $Model;
-		$this->model->validate = array_merge($this->validate, $this->model->validate);
+		$this->model->validate = am($this->validate, $this->model->validate);
 	}
 
-	public function confirmPassword($field, $password1, $password2) {
+	public function confirmPasswordNotAsEmail($field, $password1, $password2) {
+		if ($this->model->data[$this->model->alias]['password'] == $this->model->data[$this->model->alias]['email']) {
+			return false;
+		}
+        return true;
+	}
+    public function confirmPassword($field, $password1, $password2) {
 		if ($this->model->data[$this->model->alias]['password2'] == $this->model->data[$this->model->alias]['password']) {
 			return true;
 		}
+        return false;
 	}
 	
 	public function beforeSave( Model $Model ) {
